7cf97b36c5f595e8c48296a954199241b960f09e
jan.jilek <jan.jilek@alteryx.com>
2021-09-03 16:12:57 +0200

    stage 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
         {
             services.AddDbContext<DataContext>(options => { options.UseSqlite(_config.GetConnectionString("defaultConnection")); });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
